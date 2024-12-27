1b6476672f8530f2b8c8ccac9d6850e95b18363d
dora.acik <dora.acik@esensi.com.tr>
2024-12-27 14:31:40 +0300

    feature & fix : navbar added and some fixes made.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-//import NewsList from './components/NewsList'; // Haberleri göstermek için bileşen
-import NewsForm from './components/NewsForm';
-import './App.css';
-import DefaultPage from './components/DefaultPage';
-import Reports from './components/Reports';
-//import Reports from './components/Reports';<Route path="/api/news/reports" element={<Reports />} />
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NewsForm from "./components/NewsForm";
+import DefaultPage from "./components/DefaultPage";
+import Reports from "./components/Reports";
+import NavBar from "./components/Navbar";
+import "./App.css";
 
 const App = () => {
   return (
-      <Router>
-          <Routes>
-              <Route path="/" element={<DefaultPage />} />
-              <Route path="/api/news/add" element={<NewsForm />} />
-              <Route path="/api/news/reports" element={<Reports />} />
-          </Routes>
-      </Router>
+    <Router>
+      <NavBar /> 
+      <div style={{ marginTop: "200px" }}>
+        <Routes>
+          <Route path="/" element={<DefaultPage />} />
+          <Route path="/api/news/add" element={<NewsForm />} />
+          <Route path="/api/news/reports" element={<Reports />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
